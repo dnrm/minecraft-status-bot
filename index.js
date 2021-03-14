4cb31577c885ddf75a6f2c2f4fb84253332a5f2e
Daniel Medina <daniel@medina.com>
2021-03-14 09:53:35 -0600

    Improved code formatting for index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Discord = require("discord.js");
 const status = require("minecraft-server-status-improved");
 const Client = new Discord.Client();
 
-// * Message Templates
+// * Message Template
 
 const statusMessage = (response, server) => `
 **Status for "${server}"**
@@ -14,7 +14,7 @@ Online: \`${response.online}\`
 Description: \`${response.motd || "none"}\`
 Players online: \`${response.players.now}\`
 Player limit: \`${response.players.max}\`
-Minecraft Version: \`${response.server.name || 'unavailable' }\`
+Minecraft Version: \`${response.server.name || "unavailable"}\`
 `;
 
 // * Functions
@@ -22,39 +22,40 @@ Minecraft Version: \`${response.server.name || 'unavailable' }\`
 const getMessageContent = (message) => message.content.split(" ");
 
 const determineCommand = (msg, channel) => {
-  switch (msg[0]) {
-    case ".ping":
-      console.log("ping");
-      break;
+    switch (msg[0]) {
+        case ".ping":
+            console.log("ping");
+            break;
 
-    case ".status":
-      console.log("status");
-      console.log(msg[1]);
-      status(msg[1]).then((response) => {
-        channel.send(statusMessage(response, msg[1]));
-      })
-      .catch(err => {
-        console.log(err);
-      })
-      break;
+        case ".status":
+            console.log("status");
+            console.log(msg[1]);
+            status(msg[1])
+                .then((response) => {
+                    channel.send(statusMessage(response, msg[1]));
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+            break;
 
-    default:
-      console.log("unknown command");
-  }
+        default:
+            console.log("unknown command");
+    }
 };
 
 // * Events
 
 Client.on("message", (message) => {
-  let msg = getMessageContent(message);
-  console.log(msg);
-  if (msg[0].includes(".")) {
-    determineCommand(msg, message.channel);
-  }
+    let msg = getMessageContent(message);
+    console.log(msg);
+    if (msg[0].includes(".")) {
+        determineCommand(msg, message.channel);
+    }
 });
 
 Client.on("ready", () =>
-  console.log(`
+    console.log(`
     ============================
         Minecraft Status Bot
     ============================
