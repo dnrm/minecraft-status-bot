eb5edec0106c57522f1deb3d6ea2ac860d7aa519
Daniel Medina <daniel@medina.com>
2021-03-14 17:19:57 -0600

    Finished the .ping command which now responds with how much latency there is, and worked on the majority of the .config command until now. It can set a default server for the .status command, and show the value.

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const Discord = require("discord.js");
 const status = require("minecraft-server-status-improved");
 const Client = new Discord.Client();
 
+// * Variables
+
+let defaultValues = new Map();
+
 // * Message Template
 
 const statusMessage = (response, server) => `
@@ -21,20 +25,43 @@ Minecraft Version: \`${response.server.name || "unavailable"}\`
 
 const getMessageContent = (message) => message.content.split(" ");
 
-const determineCommand = (msg, channel) => {
+const determineCommand = (msg, channel, message) => {
     switch (msg[0]) {
         case ".ping":
             console.log("ping");
+            channel.send(
+                `Latency is ${Date.now() - message.createdTimestamp}ms.`
+            );
             break;
 
         case ".status":
-            status(msg[1])
-                .then((response) => {
-                    channel.send(statusMessage(response, msg[1]));
-                })
-                .catch((err) => {
-                    console.log(err);
-                });
+            if (msg[1]) {
+                getMinecraftServerData(msg[1], channel);
+            } else if (getState(getServerId(message))) {
+                getMinecraftServerData(getState(getServerId(message)), channel);
+            }
+            break;
+
+        case ".config":
+            if (msg[1] == "server" && msg[2]) {
+                modifyState(getServerId(message), msg[2]);
+                channel.send(
+                    "Successfully changed default server! Now you don't have to specify a server with the `.status` command!"
+                );
+            } else if (msg[1] == "show") {
+                channel.send(
+                    `The default status server is \`${getState(
+                        getServerId(message)
+                    )}\`.`
+                );
+            } else {
+                channel.send(
+                    msg[1]
+                        ? "Please provide a minecraft server. Example: `.config server mc.server.com`."
+                        : `Please provide an argument, such as: \`server\`, or \`show\`.`
+                );
+            }
+            logDefaultValues();
             break;
 
         default:
@@ -42,12 +69,36 @@ const determineCommand = (msg, channel) => {
     }
 };
 
+const getServerId = (message) => message.guild.id;
+
+const modifyState = (serverId, value) => {
+    defaultValues.set(serverId, value);
+};
+
+const getState = (serverId) => {
+    return defaultValues.get(serverId)
+        ? defaultValues.get(serverId)
+        : "No default server";
+};
+
+const logDefaultValues = () => console.log(defaultValues);
+
+const getMinecraftServerData = (hostname, channel) => {
+    status(hostname)
+        .then((response) => {
+            channel.send(statusMessage(response, hostname));
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+};
+
 // * Events
 
 Client.on("message", (message) => {
     let msg = getMessageContent(message);
     if (msg[0].includes(".")) {
-        determineCommand(msg, message.channel);
+        determineCommand(msg, message.channel, message);
     }
 });
 
