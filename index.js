dc8becd7a2a9647bc2d71f38820bb0e927aa96ab
Daniel Medina <daniel@medina.com>
2021-03-14 10:01:33 -0600

    Removed unused console logs

--- index.js
+++ index.js
@@ -28,8 +28,6 @@ const determineCommand = (msg, channel) => {
             break;
 
         case ".status":
-            console.log("status");
-            console.log(msg[1]);
             status(msg[1])
                 .then((response) => {
                     channel.send(statusMessage(response, msg[1]));
@@ -48,7 +46,6 @@ const determineCommand = (msg, channel) => {
 
 Client.on("message", (message) => {
     let msg = getMessageContent(message);
-    console.log(msg);
     if (msg[0].includes(".")) {
         determineCommand(msg, message.channel);
     }
